f9c457be8c596597c93909eb4a3db8311eab26d3
Maksims Mitrofanovs <A230458MM@rvt.lv>
2024-03-27 16:27:14 +0200

    Pievienota 1.1 versija

--- game.py
+++ game.py
@@ -12,7 +12,7 @@ x_final = '''
  - - -         | s |
                - - -
  '''
-x1 = '''
+x0 = '''
        _ _ _   _ _ _
        | 2 |   | 3 |
        |   |   |   |
@@ -42,10 +42,80 @@ x2 = '''
 
 '''
 
+x2_1 = '''
+       _ _ _   _ _ _
+       | g |   | 3 |
+       | i |   |   |
+ - - - - - - - |   |
+ | p |   t     |   |
+ | r | - - - - |   |
+ | i |         |   |
+ | n |         |   |
+ | t |         |   |
+ - - -         |   |
+               - - -
 
-print(x1, "Sveicinati!...")
+'''
+
+x2_1_3 = '''
+       _ _ _   _ _ _
+       | g |   | m |
+       | i |   | a |
+ - - - - - - - | i |
+ | p |   t     | n |
+ | r | - - - - | i |
+ | i |         | g |
+ | n |         | a |
+ | t |         | i |
+ - - -         | s |
+               - - -
+
+'''
+x2_1_1h = '''
+       _ _ _   _ _ _
+       | g |   | 3 |
+       | i |   |   |
+ - - - - - - - |   |
+ | p | y t h o | n |
+ | r | - - - - |   |
+ | i |         |   |
+ | n |         |   |
+ | t |         |   |
+ - - -         |   |
+               - - -
+
+'''
+
+
+
+print(x0, "Sveicinati!...")
 time.sleep(1)
 print("Jūsu jautajumi:...")
-input = ("")
-if input == "git":
+
+
+
+
+
+
+
+
+input1 = input("")
+if input1 == "git":
     print(x2)
+
+input2_1 = input("")
+if input2_1 == "print":
+    print(x2_1)
+
+input2_1_3 = input("")
+if input2_1_3 == "mainigais":
+    print(x2_1_3)
+elif input2_1_3 == "python":
+    print(x2_1_1h)
+
+input2_1_3_f = input("")
+if input2_1_3_f == "python":
+    print(x_final)
+elif input2_1_3_f == "mainigais":
+    print(x_final)
+    
