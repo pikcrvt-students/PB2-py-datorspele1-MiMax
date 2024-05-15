94bad64ca72c80459d986ff1a241ef4128a8a252
Maksims Mitrofanovs <A230458MM@rvt.lv>
2024-05-16 01:05:58 +0300

    Pievienota 3.7 versija (add array, grammar correction)

--- game_test_fix_2.py
+++ game_test_fix_2.py
@@ -2,7 +2,6 @@ import time
 import sys
 import os
 
-
 x_final = '''  
        _ _ _   _ _ _
        | g |   | s |
@@ -30,7 +29,6 @@ x0 = '''
 
 
 '''
-
 x_git = '''
        _ _ _   _ _ _
        | g |   | 3 |
@@ -45,7 +43,6 @@ x_git = '''
 
 
 '''
-
 x_git_print = '''
        _ _ _   _ _ _
        | g |   | 3 |
@@ -60,7 +57,6 @@ x_git_print = '''
 
 
 '''
-
 x_git_print_span = '''
        _ _ _   _ _ _
        | g |   | s |
@@ -89,7 +85,6 @@ x_git_print_python = '''
 
 
 '''
-
 x_git_python = '''
        _ _ _   _ _ _
        | g |   | 3 |
@@ -188,7 +183,6 @@ x_python_print = '''
                
 
 '''
-
 x_print = '''
        _ _ _   _ _ _
        | 2 |   | 3 |
@@ -231,21 +225,21 @@ x_span = '''
                
 
 '''
-Jautajumi = '''1 (vertikali) (5 burti) Kura Python funkcija tiek izmantota, lai attēlotu tekstu uz ekrāna?
-1 (horizontali) (5 burti) Kāda programmēšanas valoda tiek izmantota Django projektā?
+Jautajumi = '''1 (vertikāli) (5 burti) Kura Python funkcija tiek izmantota, lai attēlotu tekstu uz ekrāna?
+1 (horizontāli) (5 burti) Kāda programmēšanas valoda tiek izmantota Django projektā?
 2 (3 burti) Kura versiju kontroles sistēma tiek plaši izmantota, lai pārvaldītu un izsekotu izmaiņas kodā? 
-3 (4 vurti) Programmēšanas valoda, kas paredzēta Parrot virtuālajai mašīnai.'''
-Jautajums_print = '''1 (vertikali) (5 burti) Kura Python funkcija tiek izmantota, lai attēlotu tekstu uz ekrāna?'''
-Jautajums_python= '''1 (horizontali) (5 burti) Kāda programmēšanas valoda tiek izmantota Django projektā?'''
+3 (4 burti) Programmēšanas valoda, kas paredzēta Parrot virtuālajai mašīnai.'''
+Jautajums_print = '''1 (vertikāli) (5 burti) Kura Python funkcija tiek izmantota, lai attēlotu tekstu uz ekrāna?'''
+Jautajums_python= '''1 (horizontāli) (5 burti) Kāda programmēšanas valoda tiek izmantota Django projektā?'''
 Jautajums_git = '''2 (3 burti) Kura versiju kontroles sistēma tiek plaši izmantota, lai pārvaldītu un izsekotu izmaiņas kodā? '''
-Jautajums_span = '''3 (4 vurti) Programmēšanas valoda, kas paredzēta Parrot virtuālajai mašīnai.'''
+Jautajums_span = '''3 (4 burti) Programmēšanas valoda, kas paredzēta Parrot virtuālajai mašīnai.'''
 
 def game_start():
-    global Kļudu_skaits
-    Kļudu_skaits = 5
+    global Kludu_skaits
+    Kludu_skaits = 5
     os.system("cls")
-    print(x0, "Sveicinati! Šodien jūs risināsiet krustvārdu mīklu, novēlu jums veiksmi un nepieļaujiet kļūdas :)")
-    print ("Jums palika", Kļudu_skaits,"kļudas")
+    print(x0, "Sveicināti! Šodien jūs risināsiet krustvārdu mīklu, novēlu jums veiksmi un nepieļaujiet kļūdas :)")
+    print ("Jums palika", Kludu_skaits,"kļūdas")
     time.sleep(3)
     print(Jautajums_print)
     time.sleep(1)
@@ -262,15 +256,15 @@ def printPicture(xH):
 
 def ievade_code():
     global ievade
-    global Kļudu_skaits
+    global Kludu_skaits
     print("Ievadiet atbildi: ", end="")
     ievade = input("")
     while ievade != "git" and ievade != "python" and ievade != "span" and ievade != "print":
         print()
         print("Nepareizi!")
-        Kļudu_skaits -= 1
-        print("Jums palika", Kļudu_skaits,"kļudas")
-        if Kļudu_skaits == 0:
+        Kludu_skaits -= 1
+        print("Jums palika", Kludu_skaits,"kļūdas")
+        if Kludu_skaits == 0:
             print("LOSE")
             time.sleep(1)
             sys.exit()
@@ -286,7 +280,7 @@ def game_basic():
             ievade_code()
             if ievade == "span":
                 printPicture(x_git_print_span)
-            elif ievade == "python":
+            if ievade == "python":
                 printPicture(x_git_print_python)
             ievade_code()
             if ievade == "python":
@@ -436,34 +430,40 @@ def game_basic():
                 printPictirefinal(x_final)
 
 def printPictirefinal(xH):
+    global result_time
     os.system("cls")
     print(xH)
     print("Apsveicu!")
     end_time = time.time()
-    result_time = 0
     result_time = round(end_time - start_time, 3)
-    print("Jūsu laiks: ", end="")
-    print(result_time)
+    laika_saraksts.append(result_time)
+    print("Jūsu laiks:", result_time, "sekundes")
     nakamais_meginajums()
 
+laika_saraksts = [0]
+
 def nakamais_meginajums():
-    print("Jūs gribat uzlabot savu rezultatu?")
-    print()
-    print("Ja gribat, nospežat ENTER")
-    print('Ja ne, ievadiet "exit"')       
+    print("Vai gribat uzlabot savu rezultātu? (Y/N)")    
     ievade = input()
-    if ievade == "":
+    if ievade == "Y" or ievade == "y":
         global start_time
         game_start()
         start_time = time.time()
         game_basic()
         nakamais_meginajums()
-    if ievade == "exit":
-        os.system("cls")
+    else:
+        del(laika_saraksts[0])
+        elementu_skaits = len(laika_saraksts)
+        mazakais_skaitlis = laika_saraksts[0]
+        for num in range(1, elementu_skaits):
+            if laika_saraksts[num] < mazakais_skaitlis:
+                mazakais_skaitlis = laika_saraksts[num]
+        print("Jūsu rezultati: ", laika_saraksts)
+        print("Jūsu labakais laiks:", mazakais_skaitlis, "sekundes")
         print("Paldies par spelešanu!")
         sys.exit()
         
 game_start()
 start_time = time.time()
 game_basic()
-nakamais_meginajums()        
+nakamais_meginajums()
